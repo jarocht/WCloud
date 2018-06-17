cafbf666992ba6da12208292dca30590ea7b87dd
Tim Jaroch <jarocht@users.noreply.github.com>
2018-06-17 16:16:13 -0400

    Update start-appdynamics.sh

--- files/start-appdynamics.sh
+++ files/start-appdynamics.sh
@@ -1,35 +1,41 @@
 #!bin/bash
-mkdir -p /started
+echo "Start up initiated" >> appdynamics.log 
+echo "Found APPDYNAMICS_START_AGENT set to:${APPDYNAMICS_START_AGENT}" >> appdynamics.log
+if [$APPDYNAMICS_START_AGENT = 'true' ]; then
 
-pid=
-while [ -z "$pid" ]
-do
-  pid="$(pgrep java)"
-  sleep 30
-done
+    pid=
+    while [ -z "$pid" ]
+    do
+    pid="$(pgrep java)"
+    sleep 30
+    done
 
-mkdir -p /pid-${pid}
+    echo "Found PID:${pid}:" >> appdynamics.log
 
-# Determine tier name from hostname
-a=m2meventlistenerstg-v2-65785c574d-tlns7
-#a=$HOSTNAME
-b=stg
-#b=$spring_profile
+    # Determine tier name from hostname
+    a=m2meventlistenerstg-v2-65785c574d-tlns7
+    #a=$HOSTNAME
+    b=stg
+    #b=$spring_profile
 
-strindex() { 
-  x="${1%%$2*}"
-  [[ "$x" = "$1" ]] && index=-1 || index="${#x}"
-}
-strindex "$a" "$b"
-if [ $index = "-1" ]; then
-    #hostname parsing failed
-    appd_tier_name=$HOSTNAME
-else
-    appd_tier_name=${a:0:$index}
+    echo "Found HOSTNAME:${a}:" >> appdynamics.log
+    echo "Found spring_profile:${b}:" >> appdynamics.log
+
+    strindex() { 
+    x="${1%%$2*}"
+    [[ "$x" = "$1" ]] && index=-1 || index="${#x}"
+    }
+    strindex "$a" "$b"
+    if [ $index = "-1" ]; then
+        appd_tier_name=$HOSTNAME
+        echo "ERROR: Failed to parse the hostname for a service name, using hostname instead." >> appdynamics.log
+    else
+        appd_tier_name=${a:0:$index}
+    fi
+    echo "Set appd_tier_name to:${appd_tier_name}:" >> appdynamics.log
+    
+    echo "Hooking AppD agent into pid:${pid}, with tier name:${appd_tier_name}, and node name:${HOSTNAME}" >> appdynamics.log
+    java -Xbootclasspath/a:/usr/lib/jvm/java-1.8.0-openjdk-amd64/lib/tools.jar -jar /opt/appdynamics/agent/javaagent.jar ${pid} appdynamics.agent.tierName=${appd_tier_name},appdynamics.agent.nodeName=${HOSTNAME} >> appdynamics.log
+else 
+    echo "APPDYNAMICS_START_AGENT is 'false', exiting without hooking agent" >> appdynamics.log
 fi
-echo $appd_tier_name
-mkdir -p /tier_name-$appd_tier_name
-mkdir -p /node-name-$HOSTNAME
-
-
-java -Xbootclasspath/a:/usr/lib/jvm/java-1.8.0-openjdk-amd64/lib/tools.jar -jar /opt/appdynamics/agent/javaagent.jar ${pid} appdynamics.agent.tierName=${appd_tier_name},appdynamics.agent.nodeName=${HOSTNAME}
