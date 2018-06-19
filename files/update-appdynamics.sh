c612320292fce54b2f7ca7ea8f7b669912823ee0
Tim Jaroch <jarocht@users.noreply.github.com>
2018-06-18 21:03:49 -0400

    Update update-appdynamics.sh

--- files/update-appdynamics.sh
+++ files/update-appdynamics.sh
@@ -1,2 +1,6 @@
+echo "Initiated AppD update" >> appdynamics.log
+echo "Fetching start-appdynamics.sh script" >> appdynamics.log 
 wget https://raw.githubusercontent.com/jarocht/WCloud/master/files/start-appdynamics.sh -P /opt/appdynamics/
+echo "fetch complete, starting script" >> appdynamics.log 
 bash /opt/appdynamics/start-appdynamics.sh &
+echo "start-appdynamics.sh script has been started, exiting update-script.sh" >> appdynamics.log 
